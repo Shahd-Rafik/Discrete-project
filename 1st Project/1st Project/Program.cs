83f62b6370432836fd045ee6ebf8a5508f7cb8c7
Shahd-Rafik <121396738+Shahd-Rafik@users.noreply.github.com>
2022-12-27 02:21:03 +0200

    Add files via upload

---
+++ 1st Project/1st Project/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1st_Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first number:");
+            int x = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the secound number:");
+            int y = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("The prime numbers between {0} and {1} are:", x, y);
+            for (int i = x; i <= y; i++)
+            {
+                if (IsPrime(i))
+                {
+                    Console.WriteLine(i + " ");
+                }
+            }
+        }
+
+        static bool IsPrime(int number)
+        {
+            if (number < 2)
+            {
+                return false;
+            }
+
+            for (int i = 2; i <= Math.Sqrt(number); i++)
+            {
+                if (number % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ 2nd Project/2nd Project/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2nd_Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first number:");
+            int x = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number:");
+            int y = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("The perfect numbers between {0} and {1} are:", x, y);
+            for (int i = x; i <= y; i++)
+            {
+                if (IsPerfect(i))
+                {
+                    Console.WriteLine(i + " ");
+                }
+            }
+        }
+
+        private static bool IsPerfect(int number)
+        {
+            int sum = 0;
+            for (int i = 1; i < number; i++)
+            {
+                if (number % i == 0)
+                {
+                    sum += i;
+                }
+            }
+
+            return sum == number;
+        }
+    }
+}
